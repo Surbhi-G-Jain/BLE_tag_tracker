022e1c14286556023728afbbcf855094850832c2
Surbhi-G-Jain <surbhi100jain@gmail.com>
2024-12-07 23:04:37 +0530

    RSSI checking and averaing code added to check movement

--- BLE_Tag_tracker.py
+++ BLE_Tag_tracker.py
@@ -1,24 +1,36 @@
 import asyncio
 from bleak import BleakScanner
+import collections
 
-# A dictionary to store RSSI values for each detected iBeacon
-rssi_history = {}
-
+rssi_vals =collections.deque(maxlen=10)
 async def main():
+    
+    while True:
 
-    devices = await BleakScanner.discover(10.0, return_adv=True)
-    for d in devices:
-        raw_data = devices[d][1]
-        manufacturer_id=raw_data.manufacturer_data
+        devices = await BleakScanner.discover(2.0, return_adv=True)
+        for d in devices:
+            raw_data = devices[d][1]
+            manufacturer_id=raw_data.manufacturer_data
 
-        # Check for iBeacon by Manufacturer ID (76)
-        if 76 in manufacturer_id:  # Check if it's an iBeacon
-            print(f"iBeacon detected")
-            print(f"RSSI: {raw_data.rssi}")
+            # Check for manufacturer id for i beacon
+            if 76 in manufacturer_id:
+                #print(f"iBeacon detected")
+                #print(f"RSSI: {raw_data.rssi}")
+                current_rssi = raw_data.rssi
+                rssi_vals.append(current_rssi)
+                
+                if len(rssi_vals) == 10:  # Only process after we have enough data
+                    avg_rssi = sum(rssi_vals) / 10
+
+                    if current_rssi > avg_rssi + 10 or current_rssi < avg_rssi - 10:
+                        print("iBeacon is moving")
+                        
+                    else:
+                        print("iBeacon seems stationary.")
+
+
             
-           
-        else:
-            # Do nothing for non-iBeacon devices
-            continue  # Skip to the next device
+            else:
+                pass
 
 asyncio.run(main())
