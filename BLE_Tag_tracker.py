a091c340689d63268448bc70ad6a7419e2c49b94
Surbhi-G-Jain <surbhi100jain@gmail.com>
2024-12-08 00:19:15 +0530

    exceptional handling code added

--- BLE_Tag_tracker.py
+++ BLE_Tag_tracker.py
@@ -7,30 +7,51 @@ async def main():
     
     while True:
 
-        devices = await BleakScanner.discover(2.0, return_adv=True)
-        for d in devices:
-            raw_data = devices[d][1]
-            manufacturer_id=raw_data.manufacturer_data
+        try:
 
-            # Check for manufacturer id for i beacon
-            if 76 in manufacturer_id:
-                #print(f"iBeacon detected")
-                #print(f"RSSI: {raw_data.rssi}")
-                current_rssi = raw_data.rssi
-                rssi_vals.append(current_rssi)
-                
-                if len(rssi_vals) == 10:  # Only process after we have enough data
-                    avg_rssi = sum(rssi_vals) / 10
+            devices = await BleakScanner.discover(2.0, return_adv=True)
 
-                    if current_rssi > avg_rssi + 10 or current_rssi < avg_rssi - 10:
-                        print("iBeacon is moving")
+            if not devices:
+                print("No devices found")
+                continue
+
+            for d in devices:
+                try:
+                    raw_data = devices[d][1]
+                    print(raw_data)
+                    manufacturer_id=raw_data.manufacturer_data
+                    if manufacturer_id:
+                    # Check for manufacturer id for i beacon
+                        if 76 in manufacturer_id:
+                            #print(f"iBeacon detected")
+                            #print(f"RSSI: {raw_data.rssi}")
+                            current_rssi = raw_data.rssi
+                            rssi_vals.append(current_rssi)
+                            
+                            if len(rssi_vals) == 10:  # Only process after we have enough data
+                                avg_rssi = sum(rssi_vals) / 10
+
+                                if current_rssi > avg_rssi + 10 or current_rssi < avg_rssi - 10:
+                                    print("iBeacon is moving")
+                                    
+                                else:
+                                    print("iBeacon seems stationary.")
+
+
                         
+                        else:
+                            print("I beacon not found")
                     else:
-                        print("iBeacon seems stationary.")
+                        print("No manufacturer id found")
+
+                except Exception as e:
+                    print(f"Unexpected error with device {d.name}: {e}")
 
 
-            
-            else:
-                pass
+        except asyncio.CancelledError:
+            print("Scan cancelled.")
+            break  # Exit the loop if the scan is cancelled
+        except Exception as e:
+            print(f"Error during device scanning: {e}")
 
 asyncio.run(main())
